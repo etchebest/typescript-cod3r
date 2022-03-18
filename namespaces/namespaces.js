e8931a3b98dedf89fb35c02236f6ccb20a1ec810
cristian.etchebest <cristian.etchebest@meta.com.br>
2022-03-18 16:50:54 -0300

    Trabalhando com módulos

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,20 +1,25 @@
-"use strict";
 var Geometria;
 (function (Geometria) {
-    let Area;
+    var Area;
     (function (Area) {
-        const PI = 3.14;
+        var PI = 3.14;
         function circunferencia(raio) {
             return PI * Math.pow(raio, 2);
         }
         Area.circunferencia = circunferencia;
+    })(Area = Geometria.Area || (Geometria.Area = {}));
+})(Geometria || (Geometria = {}));
+var Geometria;
+(function (Geometria) {
+    var Area;
+    (function (Area) {
         function retangulo(base, altura) {
             return base * altura;
         }
         Area.retangulo = retangulo;
     })(Area = Geometria.Area || (Geometria.Area = {}));
 })(Geometria || (Geometria = {}));
-const PI = 2.99;
+/* <reference path="geometriaCirc.ts"/>
+   <reference path="geometriaRec.ts"/> */
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
-console.log(PI);
--- namespaces/namespaces.ts
+++ namespaces/namespaces.ts
@@ -1,19 +1,9 @@
-namespace Geometria {
-  export namespace Area {
-    const PI = 3.14;
-
-    export function circunferencia(raio: number): number {
-      return PI * Math.pow(raio, 2);
-    }
-    export function retangulo(base: number, altura: number): number {
-      return base * altura;
-    }
-  }
-}
-
-const PI = 2.99;
+/* <reference path="geometriaCirc.ts"/>
+   <reference path="geometriaRec.ts"/>
+   
+   rodar no terminal dentro da pasta:
+   tsc -w --outFile namespaces.js geometriaCirc.ts geometriaRec.ts namespaces.ts
+*/
 
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
-
-console.log(PI);
