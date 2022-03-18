630eb4e01668c152bad266c1c774e4e2590a92d2
cristian.etchebest <cristian.etchebest@meta.com.br>
2022-03-18 17:03:21 -0300

    instalando systemJS

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,25 +1,9 @@
-var Geometria;
-(function (Geometria) {
-    var Area;
-    (function (Area) {
-        var PI = 3.14;
-        function circunferencia(raio) {
-            return PI * Math.pow(raio, 2);
-        }
-        Area.circunferencia = circunferencia;
-    })(Area = Geometria.Area || (Geometria.Area = {}));
-})(Geometria || (Geometria = {}));
-var Geometria;
-(function (Geometria) {
-    var Area;
-    (function (Area) {
-        function retangulo(base, altura) {
-            return base * altura;
-        }
-        Area.retangulo = retangulo;
-    })(Area = Geometria.Area || (Geometria.Area = {}));
-})(Geometria || (Geometria = {}));
+"use strict";
 /* <reference path="geometriaCirc.ts"/>
-   <reference path="geometriaRec.ts"/> */
+   <reference path="geometriaRec.ts"/>
+   
+   rodar no terminal dentro da pasta:
+   tsc -w --outFile namespaces.js geometriaCirc.ts geometriaRec.ts namespaces.ts
+*/
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
